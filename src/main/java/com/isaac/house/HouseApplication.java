d5a9ecd2035446ecbab2e1b734b5fe5d96d1500b
nrainnjwa <njwasshi@gmail.com>
2023-11-10 00:57:51 +0800

    feat: add db config in app.properties

--- src/main/java/com/isaac/house/HouseApplication.java
+++ src/main/java/com/isaac/house/HouseApplication.java
@@ -2,8 +2,12 @@ package com.isaac.house;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com/isaac/house/entity"})
 public class HouseApplication {
 
 	public static void main(String[] args) {
