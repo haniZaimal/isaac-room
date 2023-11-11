50a3fb1ed6c78d07a7b0742482de9c517001c113
nrainnjwa <njwasshi@gmail.com>
2023-11-12 07:24:55 +0800

    fix: Repository annotation in ThingRepo

--- src/main/java/com/isaac/house/repository/ThingRepo.java
+++ src/main/java/com/isaac/house/repository/ThingRepo.java
@@ -2,7 +2,9 @@ package com.isaac.house.repository;
 
 import com.isaac.house.entity.Thing;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ThingRepo extends JpaRepository<Thing, Long> {
 
 
